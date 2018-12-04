51cec78d85ac5d59b25481002a8e06609d43e6b5
Michael Crispen <michaellcrispen@gmail.com>
2018-12-04 18:08:57 -0500

    Made better MakeFile

--- Graphs/Client.cpp
+++ Graphs/Client.cpp
@@ -19,7 +19,7 @@ int main() {
 		labels = "A,B,C,D,E,F,G,H,I";
 		edges = "(0,1,4)(0,7,8)(1,2,8)(1,7,11)(2,3,7)(2,5,4)(2,8,2)(3,4,9)(3,5,14)(4,5,10)(5,6,2)(6,7,1)(6,8,6)(7,8,7)";
 	}
-	
+
 
 	else {
 		printf("How many nodes will be in the graph :");
@@ -30,15 +30,15 @@ int main() {
 		printf("Please enter edges (These must be in the form %s : ", "'(Node1,Node2,Weight)'");
 		getline(cin, edges);
 	}
-	
+
 
 	Graph graph(numnodes, labels, edges);
-	
-	
+
+
 	//printf("\n");
 	//graph.PrintNodes();
 
 	//Pause before Program Exits
-	system("pause");
+	//system("pause");
 	return 0;
 }
